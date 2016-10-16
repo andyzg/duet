5639618927d9699d1d675d7ad4f581da443f6106
Gabriel Wong <me@gabrielwong.net>
2016-10-15 22:18:10 -0400

    Query more fields

---
+++ data/database.go
@@ -0,0 +1,38 @@
+package data
+
+import (
+	"time"
+)
+
+type Task struct {
+	Id        int64     `json:"id"`
+	Title     string    `json:"title"`
+	StartDate time.Time `json:"start_date"`
+	EndDate   time.Time `json:"end_date"`
+	Done      bool      `json:"done"`
+}
+
+type User struct {
+	Id int64 `json:"id"`
+}
+
+var tasks = []*Task{
+	&Task{
+		Id:        0,
+		Title:     "Get milk",
+		StartDate: time.Now(),
+		EndDate:   time.Now(),
+		Done:      false,
+	},
+	&Task{
+		Id:        1,
+		Title:     "Call my momma",
+		StartDate: time.Now(),
+		EndDate:   time.Now(),
+		Done:      true,
+	},
+}
+
+func GetTask(id int) *Task {
+	return tasks[id]
+}
--- data/schema.go
+++ data/schema.go
@@ -1,23 +1,62 @@
 package data
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/graphql/language/ast"
 )
 
 var taskType *graphql.Object
+var dateType *graphql.Scalar
 
 var Schema graphql.Schema
 
 func init() {
+	dateType = graphql.NewScalar(graphql.ScalarConfig{
+		Name:        "date",
+		Description: "Date and time",
+		Serialize: func(t interface{}) interface{} {
+			switch t := t.(type) {
+			case time.Time:
+				return t.Unix()
+			}
+			return 0
+		},
+		ParseValue: func(unix interface{}) interface{} {
+			switch unix := unix.(type) {
+			case int64:
+				return time.Unix(unix, 0)
+			}
+			return nil
+		},
+		ParseLiteral: func(valueAST ast.Value) interface{} {
+			switch valueAST := valueAST.(type) {
+			case *ast.IntValue:
+				if intValue, err := strconv.ParseInt(valueAST.Value, 10, 32); err == nil {
+					return time.Unix(intValue, 0)
+				}
+			}
+			return nil
+		},
+	})
+
 	taskType = graphql.NewObject(graphql.ObjectConfig{
 		Name:        "Task",
 		Description: "A TODO task",
 		Fields: graphql.Fields{
 			"title": &graphql.Field{
 				Type: graphql.String,
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					return "Change me", nil
-				},
+			},
+			"start_date": &graphql.Field{
+				Type: dateType,
+			},
+			"end_date": &graphql.Field{
+				Type: dateType,
+			},
+			"done": &graphql.Field{
+				Type: graphql.Boolean,
 			},
 		},
 	})
@@ -28,7 +67,7 @@ func init() {
 			"task": &graphql.Field{
 				Type: taskType,
 				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					return "Foo", nil
+					return GetTask(0), nil
 				},
 			},
 		},
