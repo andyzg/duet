a82bd5d96d20eeb09044c2aefd9f14a5ea4bacce
Gabriel Wong <me@gabrielwong.net>
2016-11-13 22:19:56 -0500

    Auth skeleton

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/ant0ine/go-json-rest/rest"
 	"github.com/gabrielwong/graphql-go-handler"
 	"github.com/mnmtanish/go-graphiql"
+
+	"golang.org/x/net/context"
 )
 
 func main() {
@@ -18,6 +20,11 @@ func main() {
 		Schema: &data.Schema,
 		Pretty: true,
 		Log:    true,
+	})
+
+	authGraphqlHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.Background()
+		graphqlHandler.ContextHandler(ctx, w, r)
 	})
 
 	restApi := rest.NewApi()
@@ -35,7 +42,7 @@ func main() {
 
 	http.HandleFunc("/", graphiql.ServeGraphiQL)
 	http.Handle("/rest/", http.StripPrefix("/rest", restApi.MakeHandler()))
-	http.Handle("/graphql", graphqlHandler)
+	http.Handle("/graphql", authGraphqlHandler)
 
 	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
